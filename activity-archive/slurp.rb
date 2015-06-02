6d8646aec042877eb1ab63bfa7b15a0ab3c9c846
Noah Paessel <knowuh@gmail.com>
2015-06-02 11:29:28 -0400

    Slurp.rb now generates index.

--- activity-archive/slurp.rb
+++ activity-archive/slurp.rb
@@ -5,6 +5,7 @@ require "rubygems"
 require "json"
 require "net/http"
 require "uri"
+require "erb"
 
 
 SERVER = "smartgraphs-authoring.staging.concord.org"
@@ -12,6 +13,7 @@ PUBLIC_ACTIVITIES_PATH = "activities.json"
 OUT_DIR = File.join(Dir.pwd, "_site")
 OUT_JSON = File.join(OUT_DIR, "public_activities.json")
 LOCAL_ACTIVITIES = File.join(OUT_DIR, "activities")
+INDEX_ERB = File.join(Dir.pwd, "index.html.erb")
 
 activities_json_url = "http://#{SERVER}/#{PUBLIC_ACTIVITIES_PATH}"
 activities_base = "http://#{SERVER}/activities/"
@@ -25,14 +27,14 @@ response = http.request(request)
 if response.code == "200"
   hash_result = JSON.parse(response.body).map do |doc|
       act = doc['activity']
-      slug = act['name'].downcase.gsub(/[^a-z]/,'_')
+      slug = act['name'].downcase.gsub(/[^a-z]+/,'-')
       { 'activity'=> {
           'author_name' => act['author_name'],
           'cc_project_name' => act['cc_project_name'],
           'id'=> act['id'],
           'name' => act['name'],
           'remote_url' => "#{activities_base}#{act['id']}/student_preview.html",
-          'local_url' => "#{act['id']}_#{slug}.html"
+          'local_url' => "#{act['id']}-#{slug}.html"
         }
     }
   end
@@ -42,19 +44,29 @@ if response.code == "200"
 
   hash_result.each do |k|
     v = k['activity']
-    puts "Key: #{k}"
-    puts "Value: #{v}"
-    puts v['remote_url']
     uri = URI.parse(v['remote_url'])
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
+    request.content_type = 'text/html'
     response = http.request(request)
     if response.code == "200"
-      File.open(File.join(OUT_DIR,v['local_url']),'w') { |f| f.write(out)}
+      File.open(File.join(OUT_DIR,v['local_url']),'w') do |f|
+        f.write(response.body)
+      end
     else
       puts "Error slurping #{v['local_url']}"
       puts $!
     end
+  end
+
+  activities = hash_result.map { |a| a['activity'] }.sort { |a,b|
+    a['name'].downcase <=> b['name'].downcase
+  }
+  template = ERB.new(File.read(INDEX_ERB))
+  index_content = template.result()
+
+  File.open(File.join(OUT_DIR, 'index.html'),'w') do |f|
+    f.write(index_content)
   end
 
 else
